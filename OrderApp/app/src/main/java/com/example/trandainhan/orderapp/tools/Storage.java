6b35049590a441b6f5f606c4de48d6db01086d88
DuyThanhNguyen <thanhtdt@outlook.com>
2017-05-05 23:05:05 +0700

    ok

---
+++ OrderApp/app/src/main/java/com/example/trandainhan/orderapp/tools/Storage.java
@@ -0,0 +1,10 @@
+package com.example.trandainhan.orderapp.tools;
+
+/**
+ * Created by duyth on 5/5/2017.
+ */
+
+public class Storage {
+    public static final String Username = null;
+    public static final String Password = null;
+}
